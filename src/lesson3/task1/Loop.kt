6447cf9fa3ba6b90ac7f44a5af802a121b1ea5d5
DANY211 <DANY@github.com>
2017-11-03 18:11:23 +0300

    i do something

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -1,6 +1,9 @@
 @file:Suppress("UNUSED_PARAMETER")
 package lesson3.task1
 
+import java.lang.Math.round
+import java.lang.Math.sqrt
+
 /**
  * Пример
  *
@@ -60,7 +63,7 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  * Найти количество цифр в заданном числе n.
  * Например, число 1 содержит 1 цифру, 456 -- 3 цифры, 65536 -- 5 цифр.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int = n.toString().length
 
 /**
  * Простая
@@ -68,7 +71,18 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+    if ( n in 1..2 ) return 1
+    var result = 0
+    var a = 1
+    var b = 1
+    for ( i in 3..n ) {
+        result = a + b
+        a = b
+        b = result
+    }
+    return result
+}
 
 /**
  * Простая
@@ -76,21 +90,45 @@ fun fib(n: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+
+fun lcm(m: Int, n: Int): Int {
+    val prod = m * n
+    var a = m
+    var b = n
+    while ( a != b ) {
+
+        if ( a > b ) {
+            a -= b
+        }
+        else {
+            b -= a
+        }
+    }
+    return prod / a
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    var a = 2
+    while ( n % a != 0 ) a++
+    return a
+}
+
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var a = n - 1
+    while ( n % a != 0 ) a--
+    return a
+}
 
 /**
  * Простая
@@ -99,7 +137,16 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    var a = m
+    var b = n
+    while ( b != 0 ) {
+        val c = a % b
+        a = b
+        b = c
+    }
+    return a == 1
+}
 
 /**
  * Простая
@@ -108,8 +155,16 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
-
+fun squareBetweenExists(m: Int, n: Int): Boolean {
+    val a = sqrt(m.toDouble())
+    val b = sqrt(n.toDouble())
+    val difference = b.toInt() - a.toInt()
+    if ( m == n ) {
+        val roundM = round(a).toInt()
+        if ( roundM * roundM == m ) return true
+    }
+    return ( difference >= 1 )
+}
 /**
  * Средняя
  *
@@ -117,7 +172,21 @@ fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
  * sin(x) = x - x^3 / 3! + x^5 / 5! - x^7 / 7! + ...
  * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
  */
-fun sin(x: Double, eps: Double): Double = TODO()
+fun sin(x: Double, eps: Double): Double {
+    var a: Double
+    var sin: Double
+    var b = 3.0
+    val xx = x % ( 2 * Math.PI )
+    sin = xx
+    a = xx
+    while ( Math.abs(a) > eps ) {
+        a = a * xx * xx / b / ( b - 1 ) * ( -1 )
+        b += 2
+        sin += a
+    }
+    return sin
+}
+
 
 /**
  * Средняя
@@ -126,7 +195,21 @@ fun sin(x: Double, eps: Double): Double = TODO()
  * cos(x) = 1 - x^2 / 2! + x^4 / 4! - x^6 / 6! + ...
  * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
  */
-fun cos(x: Double, eps: Double): Double = TODO()
+fun cos(x: Double, eps: Double): Double {
+    var a: Double
+    var cos: Double
+    var b = 2.0
+    cos = 1.0
+    a = 1.0
+    val xx = x % ( 2 * Math.PI )
+    while ( Math.abs(a) > eps ) {
+        a = a * xx * xx / b / ( b - 1 ) * ( -1 )
+        b += 2
+        cos += a
+    }
+    return cos
+}
+
 
 /**
  * Средняя
@@ -134,7 +217,15 @@ fun cos(x: Double, eps: Double): Double = TODO()
  * Поменять порядок цифр заданного числа n на обратный: 13478 -> 87431.
  * Не использовать строки при решении задачи.
  */
-fun revert(n: Int): Int = TODO()
+fun revert(n: Int): Int {
+    var a = 0
+    var b = n
+    while ( b > 0 ) {
+        a = ( a * 10 ) + b % 10
+        b /= 10
+    }
+    return a
+}
 
 /**
  * Средняя
@@ -143,7 +234,20 @@ fun revert(n: Int): Int = TODO()
  * первая цифра равна последней, вторая -- предпоследней и так далее.
  * 15751 -- палиндром, 3653 -- нет.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
+fun isPalindrome(n: Int): Boolean {
+    var a = listOf<Int>()
+    var b = n
+    do {
+        a += b % 10
+        b /= 10
+    }
+    while (b != 0)
+    for ( i in 0..a.size - 1 ) {
+        if ( a[i] != a[a.size - i - 1] ) return false
+    }
+    return true
+}
+
 
 /**
  * Средняя
@@ -151,7 +255,18 @@ fun isPalindrome(n: Int): Boolean = TODO()
  * Для заданного числа n определить, содержит ли оно различающиеся цифры.
  * Например, 54 и 323 состоят из разных цифр, а 111 и 0 из одинаковых.
  */
-fun hasDifferentDigits(n: Int): Boolean = TODO()
+fun hasDifferentDigits(n: Int): Boolean {
+    val a = n % 10
+    var b = n
+    do {
+        if ( a != b % 10 ) {
+            return true
+        }
+        b /= 10
+    }
+    while ( b != 0 )
+    return false
+}
 
 /**
  * Сложная
@@ -160,7 +275,18 @@ fun hasDifferentDigits(n: Int): Boolean = TODO()
  * 149162536496481100121144...
  * Например, 2-я цифра равна 4, 7-я 5, 12-я 6.
  */
-fun squareSequenceDigit(n: Int): Int = TODO()
+fun squareSequenceDigit(n: Int): Int {
+    var a = "1"
+    var b = 2
+    var c = 0
+    while ( c + a.length < n ) {
+        c += a.length
+        a = ( b * b ).toString()
+        b += 1
+    }
+    return ( a[n - c - 1] - '0' ).toInt()
+}
+
 
 /**
  * Сложная
